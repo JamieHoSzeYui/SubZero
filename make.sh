4ded0a2ac3bafda038965b16cec475f1b590be47
JamieHoSzeYui <henloboii@protonmail.ch>
2020-11-26 16:19:35 +0800

    subzero is so cool

--- make.sh
+++ make.sh
@@ -27,12 +27,24 @@ fi
 mkdir drive
 sudo mount $MOUNTP drive   
 if [[ $(ls drive/Windows/System32 | grep cmd.exe) ]]; then 
-    echo "System32 detected. Continuing."
+    :
 else
     echo "System32 not found. Are you at the right directory?"
     exit 
 fi 
-
+echo "============================================================================="
+echo " ______     __  __     ______     ______     ______     ______     ______    "  
+echo "/\  ___\   /\ \/\ \   /\  == \   /\___  \   /\  ___\   /\  == \   /\  __ \   "  
+echo "\ \___  \  \ \ \_\ \  \ \  __<   \/_/  /__  \ \  __\   \ \  __<   \ \ \/\ \  "
+echo " \/\_____\  \ \_____\  \ \_____\   /\_____\  \ \_____\  \ \_\ \_\  \ \_____\ "
+echo "  \/_____/   \/_____/   \/_____/   \/_____/   \/_____/   \/_/ /_/   \/_____/ "
+echo "============================================================================="
+echo "       Release R1 . For the freedom we have been asking for. @Elation232     "
+echo "============================================================================="
+echo ""
+echo ""
+echo ""
+echo ""
 # Logic parts
 cd drive/Windows/System32 
 sudo cp -fpr osk.exe wtf.exe 
